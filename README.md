0e5b98111ecb31048daf76952b16cd3421d99e39
u9521 <63995396+u9521@users.noreply.github.com>
2025-05-17 18:20:24 +0800

    update(module): update readme add(module): update signature

--- README.md
+++ README.md
@@ -1,48 +1,57 @@
 <div align="center">
-   <img width="160" src="https://raw.githubusercontent.com/Simplicity-Team/WooBoxForColorOS/main/doc/ic_launcher.png" alt="logo">
+   <img width="160" src="https://raw.githubusercontent.com/u9521/WooBoxForRedmagicOS/main/doc/ic_launcher.png" alt="logo">
    <h1>WooBox For ColorOS</h1>
    <p>
-       <b><a href="https://github.com/Simplicity-Team/WooBoxForColorOS/blob/main/README_EN.md">English</a>  丨 简体中文</b>
+       <b><a href="https://github.com/u9521/WooBoxForRedmagicOS/blob/main/README_EN.md">English</a>  丨 简体中文</b>
    </p>
-   <a href="https://github.com/Xposed-Modules-Repo/wooboxforredmagicos/releases"><img alt="GitHub all releases" src="https://img.shields.io/github/downloads/Xposed-Modules-Repo/wooboxforredmagicos/total?label=Downloads"></a>
-   <a href="https://github.com/Simplicity-Team/WooBoxForColorOS/stargazers"><img alt="GitHub stars" src="https://img.shields.io/github/stars/Simplicity-Team/WooBoxForColorOS"></a>
-   <a href="https://github.com/Simplicity-Team/WooBoxForColorOS/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/Simplicity-Team/WooBoxForColorOS"></a>
-   <a href="https://github.com/Simplicity-Team/WooBoxForColorOS/blob/main/LICENSE"><img alt="GitHub license" src="https://img.shields.io/github/license/Simplicity-Team/WooBoxForColorOS"></a>
-   <p>一个基于 ColorOS12（Android12）适配的系统拓展Xposed模块</p>
+   <a href="https://github.com//u9521/WooBoxForRedmagicOS/stargazers"><img alt="GitHub stars" src="https://img.shields.io/github/stars/u9521/WooBoxForRedmagicOS"></a>
+   <a href="https://github.com/u9521/WooBoxForRedmagicOS/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/u9521/WooBoxForRedmagicOS"></a>
+   <a href="https://github.com/u9521/WooBoxForRedmagicOS/blob/main/LICENSE"><img alt="GitHub license" src="https://img.shields.io/github/license/u9521/WooBoxForRedmagicOS"></a>
+   <p>一个基于 RedmagicOS 9(Android 14)适配的系统拓展Xposed模块</p>
 </div>
 
 ---
 
-# 正在开发，很快不会上线
 
 ### 模块说明
 
-仅支持 ColorOS12（Android12），其余版本无法使用。  
+仅支持 RedmagicOS 9(Android 14)，其余版本无法使用。
   
-开发者：[酷安@乌堆小透明](http://www.coolapk.com/u/883441)  
+开发者：[@u9521](https://github.com/u9521)
+
+原开发者：[酷安@乌堆小透明](http://www.coolapk.com/u/883441)
 
 ---
 
-### 模块截图
+### 模块截图 ~~ （传家宝截图，以后再更新） ~~
 ![截图](https://raw.githubusercontent.com/Simplicity-Team/WooBoxForColorOS/main/doc/cn.jpg)  
 
 ---
 
 ### 下载地址
 
-Xposed仓库下载：[点击此处下载最新发行版](https://github.com/Xposed-Modules-Repo/wooboxforredmagicos/releases)  
+仓库Release下载(目前唯一发布渠道)：[releases](https://github.com/WooBoxForRedmagicOS/releases)
 
-酷安下载：[点击此处下载最新发行版](https://www.coolapk.com/apk/wooboxforredmagicos)
+~~Xposed仓库下载：[点击此处下载最新发行版](https://github.com/Xposed-Modules-Repo/wooboxforredmagicos/releases)~~
+
+~~酷安下载：[点击此处下载最新发行版](https://www.coolapk.com/apk/wooboxforredmagicos)~~
 
 ---
 
 ### 第三方开源引用
 ##### GNU General Public License v3.0
-[qqlittleice/MiuiHome](https://github.com/qqlittleice/MiuiHome)  
-[yujincheng08/BiliRoaming](https://github.com/yujincheng08/BiliRoaming)  
-[Mikanoshi/CustoMIUIzer](https://code.highspec.ru/Mikanoshi/CustoMIUIzer)  
+[qqlittleice/MiuiHome](https://github.com/qqlittleice/MiuiHome)
+
+[yujincheng08/BiliRoaming](https://github.com/yujincheng08/BiliRoaming)
+
+[Mikanoshi/CustoMIUIzer](https://code.highspec.ru/Mikanoshi/CustoMIUIzer)
+
+[LSPosed/DisableFlagSecure](https://github.com/LSPosed/DisableFlagSecure)
+
 ##### GNU General Public License v2.0
-[LSPosed/CorePatch](https://github.com/LSPosed/CorePatch)  
+~~[LSPosed/CorePatch](https://github.com/LSPosed/CorePatch)~~(不再集成，有需要点击链接下载)
+##### GNU Lesser General Public License v3.0
+[LuckyPray/DexKit](https://github.com/LuckyPray/DexKit)
 ##### GNU Lesser General Public License v2.1
 [577fkj/blockmiui](https://github.com/577fkj/blockmiui)  
 ##### Apache License 2.0
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,51 @@
 import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
 }
 
+fun getSigningProperties(): Properties {
+    return Properties().apply {
+        val propertiesFile = rootProject.file("sign.properties")
+        if (propertiesFile.exists()) {
+            load(FileInputStream(propertiesFile))
+        }
+    }
+}
+
+
 android {
     namespace = "com.u9521.wooboxforredmagicos"
     signingConfigs {
-        create("release") {}
+        getByName("debug") {
+            storeFile = rootProject.file("keystore/testkey.p12")
+            storePassword = "android"
+            keyAlias = "androidtestkey"
+            keyPassword = "android"
+            enableV1Signing = true
+            enableV2Signing = true
+            enableV3Signing = true
+        }
+        create("release") {
+            val props = getSigningProperties()
+            val keystorePath = props.getProperty("signing.storeFile")
+            val keystoreFile = keystorePath?.let { rootProject.file(it) }
+            if (keystoreFile?.exists() == true) {
+                storeFile = keystoreFile
+                storePassword = props.getProperty("signing.storePassword", "")
+                keyAlias = props.getProperty("signing.keyAlias", "")
+                keyPassword = props.getProperty("signing.keyPassword", "")
+                enableV1Signing = true
+                enableV2Signing = true
+                enableV3Signing = true
+            } else {
+                initWith(getByName("debug"))
+                println("\u001B[33m[Warning]: Release signing config not found, falling back to debug signing\u001B[0m")
+            }
+        }
     }
     compileSdk = 35
 //    buildToolsVersion = "32.0.0"
@@ -21,22 +58,31 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
+        ndk {
+            abiFilters.add("arm64-v8a")
+        }
     }
 
     buildTypes {
-        all {
-            signingConfig = signingConfigs.getByName("debug")
-        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
             setProguardFiles(
                 listOf("proguard-rules.pro")
             )
+            signingConfig = signingConfigs.getByName("release")
+
         }
         debug {
             isMinifyEnabled = false
             isShrinkResources = false
+            isJniDebuggable = true
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("debug")
+            ndk {
+                abiFilters.clear()
+                abiFilters.addAll(listOf("arm64-v8a", "x86_64"))
+            }
         }
     }
     compileOptions {
@@ -51,17 +97,53 @@ android {
         resources {
             excludes += "/META-INF/**"
             excludes += "/kotlin/**"
-            excludes += "/*.txt"
-            excludes += "/*.bin"
         }
         dex {
             useLegacyPackaging = true
         }
     }
     applicationVariants.all {
-        outputs.all {
-            (this as BaseVariantOutputImpl).outputFileName =
-                "WooBoxForColorOS-$versionName-$name.apk"
+        outputs.forEach { output ->
+            var outPrefix = "WooBox4RMOS-${buildType.name}-${versionName}(${versionCode})"
+            val abi = buildTypes.getByName(buildType.name).ndk.abiFilters
+            if (abi.isNotEmpty()) {
+                abi.forEach {
+                    outPrefix += "-$it"
+                }
+            }
+            outPrefix += ".apk"
+            (output as BaseVariantOutputImpl).outputFileName = outPrefix
+        }
+        this.assembleProvider?.configure {
+            doLast {
+                val outputDir = File(project.rootDir, "outputs/${buildType.name}")
+                outputDir.deleteRecursively()
+                outputDir.mkdirs()
+                val apkDir = layout.buildDirectory.dir("outputs/apk/${buildType.name}").get().asFile
+                if (apkDir.exists()) {
+                    apkDir.listFiles()?.forEach { file ->
+                        val destFile = File(outputDir, file.name)
+                        if (destFile.name == "baselineProfiles") {
+                            return@forEach
+                        }
+                        file.copyTo(destFile, overwrite = true)
+                        println("Copied apk file to: ${destFile.absolutePath}")
+                    }
+                }
+                val mappingDir =
+                    layout.buildDirectory.dir("outputs/mapping/${buildType.name}").get().asFile
+                if (mappingDir.exists()) {
+                    mappingDir.listFiles()?.forEach { file ->
+                        var destFile = File(outputDir, file.name)
+                        if (destFile.name == "mapping.txt") {
+                            destFile = File(outputDir, "mapping-${versionName}(${versionCode}).txt")
+                        }
+                        file.copyTo(destFile, overwrite = true)
+                        println("Copied mapping file to: ${destFile.absolutePath}")
+                    }
+                }
+
+            }
         }
     }
 }
@@ -75,8 +157,4 @@ dependencies {
     //UI
     implementation(project(":blockmiui"))
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    //APP Center
-//    val appCenterSdkVersion = "5.0.6"
-//    implementation("com.microsoft.appcenter:appcenter-analytics:${appCenterSdkVersion}")
-//    implementation("com.microsoft.appcenter:appcenter-crashes:${appCenterSdkVersion}")
 }
