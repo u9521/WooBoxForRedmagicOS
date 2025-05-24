9fbc15f0d4dff0c833a0e92bc4eaef6bf1815734
u9521 <63995396+u9521@users.noreply.github.com>
2025-05-24 16:03:11 +0800

    update app version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,8 @@ android {
         applicationId = "com.u9521.wooboxforredmagicos"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
